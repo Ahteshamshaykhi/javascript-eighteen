32c6eae5105ab37aeb4561824e2ca5ba6bdc2594
unknown <ahteshamshaykhi@gmail.com>
2024-03-24 07:49:19 +0500

    readable

---
+++ app.js
@@ -0,0 +1,85 @@
+// changing case
+
+//Q1
+
+//var allowercase = userinput.toLowerCase;
+
+//Q2
+
+//var x = x.toLowerCase();
+
+//Q3
+
+//var y = y.toUpperCase();
+
+
+//Q4
+
+var firstString = "Ahmed"
+var Secondstring = firstString.toLowerCase();
+console.log(Secondstring);
+
+//Q5
+
+var a = ["Element1","Element2","Element3"];
+var b = a[0].toLowerCase();
+
+var index = 1;
+var f = a[index].toLowerCase();
+console.log(f);
+
+//Q6
+
+var h = "ahteshamahmed"
+var t = h.toUpperCase();
+
+alert(t);
+
+
+//Q7
+
+var cityName = "kaRaCHi"
+var g = cityName.charAt(0).toUpperCase()+cityName.slice(1).toLowerCase();
+
+console.log(g);
+
+//String methods
+
+//Q1
+
+// var Words = "captain";
+// var string = sameWords.slice(1, 3);
+
+// console.log(string);
+
+//Q2
+
+var one = "AhteshamSheikh"
+var two = one.length;
+console.log(two);
+
+
+//Q3
+
+var animal = "elephant";
+var m = animal.slice(3,6)
+console.log(m);
+
+//Q4
+
+var num1 = "AlphaBetaGamarays";
+var num2 = num1.length;
+console.log(num2)
+
+//Q5
+
+
+
+
+
+
+
+
+
+
+
